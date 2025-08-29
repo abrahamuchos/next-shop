f60af36812e78069cadb7cd219c3027f7a34a098
abrahamuchos <j.abraham29@gmail.com>
2025-08-29 11:33:30 -0400

    Add hero component

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import styles from './Hero.module.scss';
 
 export default function Hero() {
+
   return (
     <section className={styles.Hero}>
-      <h1>Hero</h1>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
   );
 }
